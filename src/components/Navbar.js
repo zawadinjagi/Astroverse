9ad007db29f46e9294a823fe4097938edda91b72
zawadinjagi <zawadinjagi619@gmail.com>
2024-05-13 12:58:28 +0300

    linking the solar system part to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,18 @@
 import React from "react";
 import styles from "./Navbar.module.css";
-import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { SiNasa } from "react-icons/si";
-import { FiCodepen } from "react-icons/fi";
-import { FaReact } from "react-icons/fa";
+
 
 const Navbar = () => {
   const navbarLinks = [
-    {
-      href: "https://github.com/gupta-piyush19",
-      icon: <BsGithub />,
-      label: "GitHub",
-    },
-    {
-      href: "https://www.linkedin.com/in/piyushgupta19/",
-      icon: <BsLinkedin />,
-      label: "Linkedin",
-    },
+ 
+   
     {
       href: "https://api.nasa.gov/",
       icon: <SiNasa />,
       label: "NASA",
     },
-    {
-      href: "https://piyushgupta.vercel.app/",
-      icon: <FaReact />,
-      label: "Portfolio",
-    },
-    {
-      href: "https://codepen.io/Piyush0_0",
-      icon: <FiCodepen />,
-      label: "Codepen",
-    },
+ 
   ];
   return (
     <nav className={styles.navbar}>
